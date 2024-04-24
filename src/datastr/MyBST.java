283beb61f316307dac30a8f57fd5b226fc556564
localuser <localuser@VEA.LV>
2024-04-24 11:24:52 +0300

    a lot of stuff done here

---
+++ src/datastr/MyBST.java
@@ -0,0 +1,73 @@
+package datastr;
+
+public class MyBST<T> {
+
+    private MyTreeNode<T> root = null;
+    private int counter = 0;
+
+    public boolean isEmpty() {
+        return (counter == 0);
+    }
+
+    public int howManyElements() {
+        return counter;
+    }
+
+    public void add(T element) throws Exception {
+        if (element == null) throw new Exception("Element is null");
+
+        if (isEmpty()) {
+            root = new MyTreeNode<>(element);
+            counter++;
+        } else {
+            insertHelperRecursive(root, element);
+        }
+
+    }
+
+    private void insertHelperRecursive(MyTreeNode<T> node, T element) {
+        if (((Comparable)node.getElement()).compareTo(element) == 1) {
+            if (node.getLeft() == null) {
+                node.setLeft(new MyTreeNode<>(element));
+                node.getLeft().setParent(node);
+                counter++;
+            } else {
+                insertHelperRecursive(node.getLeft(), element);
+            }
+        } else if (((Comparable)node.getElement()).compareTo(element) == -1) {
+            if (node.getRight() == null) {
+                node.setRight(new MyTreeNode<>(element));
+                node.getRight().setParent(node);
+                counter++;
+            } else {
+                insertHelperRecursive(node.getRight(), element);
+            }
+        } else {
+            System.out.println("Element " + element + " is already in the tree");
+        }
+    }
+
+    public void print() throws Exception {
+        if (isEmpty()) throw new Exception("Tree is empty");
+
+        printHelperRecursivePreOrder(root);
+    }
+
+    private void printHelperRecursivePreOrder(MyTreeNode<T> node) {
+        System.out.println("P: " + node.getElement());
+
+        if (node.getLeft() != null) {
+            System.out.println(" -> LC: " + node.getLeft().getElement()
+                    + " [ " + node.getElement() + " ] ");
+            printHelperRecursivePreOrder(node.getLeft());
+        }
+
+        if (node.getRight() != null) {
+            System.out.println(" -> RC: " + node.getRight().getElement()
+                    + " [ " + node.getElement() + " ] ");
+            printHelperRecursivePreOrder(node.getRight());
+        }
+    }
+
+
+}
--- src/datastr/MyTreeNode.java
+++ src/datastr/MyTreeNode.java
@@ -1,4 +1,47 @@
 package datastr;
 
-public class MyTreeNode {
+import java.util.Objects;
+
+public class MyTreeNode<T> {
+
+    private T element;
+    private MyTreeNode<T> left = null;
+    private MyTreeNode<T> right = null;
+    private MyTreeNode<T> parent = null;
+
+    public MyTreeNode(T element) {
+        setElement(element);
+    }
+
+    public T getElement() {
+        return element;
+    }
+
+    public MyTreeNode<T> getLeft() {
+        return left;
+    }
+
+    public MyTreeNode<T> getRight() {
+        return right;
+    }
+
+    public MyTreeNode<T> getParent() {
+        return parent;
+    }
+
+    public void setElement(T element) {
+        this.element = Objects.requireNonNullElseGet(element, () -> (T) new Object());
+    }
+
+    public void setLeft(MyTreeNode<T> left) {
+        this.left = left;
+    }
+
+    public void setRight(MyTreeNode<T> right) {
+        this.right = right;
+    }
+
+    public void setParent(MyTreeNode<T> parent) {
+        this.parent = parent;
+    }
 }
